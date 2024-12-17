f51c13778a5a6efa519e525243264f2cc080f12b
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-12-17 14:07:19 +0200

    fix(TaxonomyPicker): Fixed an issue that caused a crash if the taxonomy picker is used to search for values. 🐛

--- blocks/shared/components/TaxonomyPicker.js
+++ blocks/shared/components/TaxonomyPicker.js
@@ -21,6 +21,18 @@ import useTaxonomy from "../useTaxonomy";
 export default function TaxonomyPicker({ taxonomySlug, onChange, value }) {
 	const [taxonomyList, setTaxonomyList] = useState([]);
 	const [visibleTaxonomyList, setVisibleTaxonomyList] = useState({});
+	const [internalValue, setInternalValue] = useState([]);
+
+	useEffect(() => {
+		// Here we go through the currently selected IDs and map them to the "string" value id###name
+		if (value && Object.keys(visibleTaxonomyList).length > 0) {
+			const mappedValues = value.map((v) => {
+				const found = visibleTaxonomyList[v];
+				return found ? `${v}###${found.name}` : undefined;
+			}).filter((v) => v !== undefined);
+			setInternalValue(mappedValues);
+		}
+	}, [value, visibleTaxonomyList]);
 
 	const { taxonomy, loading: loadingTaxonomy } = useTaxonomy(taxonomySlug);
 	const { taxonomyRecords, loading: loadingRecords } = useTaxonomyRecords(
@@ -32,18 +44,28 @@ export default function TaxonomyPicker({ taxonomySlug, onChange, value }) {
 
 	useEffect(() => {
 		if (taxonomyRecords) {
-			const mappedRecords = taxonomyRecords.map((record) => record.id);
+			const mappedRecords = taxonomyRecords.map((record) => `${record.id}###${record.name}`);
 			const mappedVisibleRecord = {};
 			taxonomyRecords.forEach((term) => {
-				mappedVisibleRecord[term.id] = {
+				mappedVisibleRecord[term.id.toString()] = {
 					slug: term.slug,
 					name: term.name,
 				};
 			});
+			// Used for the suggestions
 			setTaxonomyList(mappedRecords);
+			// This is used to map the currently selected values
+			// (ids) -> to the correct internal values (id###name)
 			setVisibleTaxonomyList(mappedVisibleRecord);
 		}
 	}, [taxonomyRecords]);
+
+	// Converts the selected values (id###name) to just the id. (filters out "empty" values)
+	const handleChange = (values) => {
+		const mappedValues = values.map((value) => value.split("###")[0]).filter((value) => value !== "" && value !== undefined && value !== null && value !== false && value !== 'undefined');
+		const mappedRecords = mappedValues.map((value) => value ?? undefined).filter((value) => value !== undefined);
+		onChange(mappedRecords);
+	}
 
 	return (
 		<Flex>
@@ -53,17 +75,12 @@ export default function TaxonomyPicker({ taxonomySlug, onChange, value }) {
 					__next40pxDefaultSize
 					__nextHasNoMarginBottom
 					displayTransform={(token) => {
-						if (visibleTaxonomyList[token]) {
-							return visibleTaxonomyList[token].name;
-						}
-						return token;
-					}}
-					saveTransform={(token) => {
-						return token + "";
+						const [_id, name] = token.split("###");
+						return name ?? "";
 					}}
 					suggestions={taxonomyList}
-					value={value}
-					onChange={onChange}
+					value={internalValue}
+					onChange={handleChange}
 					label={`${sprintf(
 						// translators: The label of the taxonomy.
 						_x(
