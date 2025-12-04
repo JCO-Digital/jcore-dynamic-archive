ab85d3efc0f53f7024f54432e0b9c747c107ee66
Johan Österberg <johan.osterberg@jco.fi>
2025-12-04 18:38:33 +0200

    feat: Support taxonomy filter by slug and customizable param names
    - Add get_taxonomy_param_field_type helper and filter - Allow taxonomy
    filters to use term slugs as param values - Make build_param_name
    filterable for custom param naming - Update JS and Twig templates to use
    new param naming and field type - Replace 'paged' param with
    'archive-paged' for pagination

--- blocks/build/dynamic-archive/view.asset.php
+++ blocks/build/dynamic-archive/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => '77f0fa29cb0ee4d4a42d', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity', array('id' => '@wordpress/interactivity-router', 'import' => 'dynamic')), 'version' => 'a019318f753116853d23', 'type' => 'module');
