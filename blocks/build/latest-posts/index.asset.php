1bac7369a2c146a09e7fdf455046cc26b14eee5c
Maxemilian Grönblom <maxemiliang@gmail.com>
2026-04-02 13:32:44 +0300

    chore: build artifacts

--- blocks/build/latest-posts/index.asset.php
+++ blocks/build/latest-posts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '71a8b324fa6523167f74');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'affce3803cfdc78a90af');
